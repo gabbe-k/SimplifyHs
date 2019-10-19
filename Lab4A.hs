6d18411e703d3741d3f896c21de6c11bca4b8498
gabbe-k <44163424+gabbe-k@users.noreply.github.com>
2019-10-19 12:35:17 +0200

    Update Lab4A.hs

--- Lab4A.hs
+++ Lab4A.hs
@@ -11,12 +11,7 @@ data BinOp = AddOp | MulOp
 
 --------------------------------------------------------------------------------
 -- * A1
--- Define a data type Expr which represents three kinds of expression:
--- binary operators (use BinOp as a helper type) applied to two expressions,
--- numbers (use Int), and exponentiation x^n.
--- Note that since we consider expressions containing just a single variable,
--- x, your data type should not use String or Char anywhere, since this is
--- not needed.
+-- Data type for expressions
 
 data Expr = N Int 
           | X Int     
@@ -26,7 +21,7 @@ data Expr = N Int
 
 --------------------------------------------------------------------------------
 -- * A2
--- Define the data type invariant that checks that exponents are never negative
+-- Checks that exponents are never negative in an expression
 prop_Expr :: Expr -> Bool
 prop_Expr (BExp op e1 e2) = (prop_Expr e1) && (prop_Expr e2)
 prop_Expr (X n)            = not(n < 0)
@@ -34,12 +29,9 @@ prop_Expr (N n)            = True
 
 --------------------------------------------------------------------------------
 -- * A3
--- Make Expr an instance of Show (along the lines of the example in the lecture)
--- You can use Haskell notation for powers: x^2
--- You should show x^1 as just x. 
+-- Shows an expresion
 
 instance Show Expr where
-  show (X 0)                  = "1"
   show (X 1)                  = "x"
   show (X n)                  = "(x^" ++ (show n) ++ ")"
   show (N n)                  = (show n)
@@ -49,25 +41,11 @@ instance Show Expr where
 
 --------------------------------------------------------------------------------
 -- * A4
--- Make Expr and instance of Arbitrary.
--- Now you can check the data type invariant that you defined in A3 using
--- quickCheck
-
--- (Optional)
--- Add a definition of function shrink :: Expr -> [Expr] to Arbitrary
--- which gives hints to quickCheck on possible smaller expressions that it
--- could use to find a smaller counterexample for failing tests
-
---not final
---maybe negative nums 
--- rSingle :: Gen Expr
--- rSingle = do r <- choose(0,10)
---              n <- elements[N,X]
---              return (n r)
+-- Generates random expressions
 
 rSingle :: Gen Expr
-rSingle = do exp <- choose(1,9)
-             n <- choose(1,100)
+rSingle = do exp <- choose(0,9)
+             n <- choose(0,100)
              t <- elements[(X 0),(N 0)]
              case t of
               (X _) -> (return(X exp))
@@ -88,9 +66,7 @@ instance Arbitrary Expr where arbitrary = do r <- choose (0,5)
 
 --------------------------------------------------------------------------------
 -- * A5
--- Define the eval function which takes a value for x and an expression and
--- evaluates it
-
+-- Takes a value for x and an expression and evaluates it
 eval :: Int -> Expr -> Int
 eval x (X n)           = x ^ n 
 eval x (N n)           = n 
@@ -100,65 +76,80 @@ eval x (BExp op e1 e2) = (opA op) (eval x e1) (eval x e2)
   
 --------------------------------------------------------------------------------
 -- * A6
--- Define
+-- Converts expression to polynomial
 exprToPoly :: Expr -> Poly
-
 exprToPoly (N n)                      = fromList [n]
 exprToPoly (X n)                      = fromList (1 : replicate n 0)
 exprToPoly (BExp AddOp e1 e2)         = (exprToPoly e1) + (exprToPoly e2)
 exprToPoly (BExp MulOp e1 e2)         = (exprToPoly e1) * (exprToPoly e2)
         
--- Define (and check) prop_exprToPoly, which checks that evaluating the
--- polynomial you get from exprToPoly gives the same answer as evaluating
--- the expression
 
+-- Checks that evaluating the polynomial you get from exprToPoly gives
+-- the same answer as evaluating the expression
 prop_exprToPoly :: Expr -> Int -> Bool
 prop_exprToPoly e x = (eval x e) == evalPoly x (exprToPoly e)
 
 --------------------------------------------------------------------------------
 -- * A7
--- Now define the function going in the other direction, 
-polyToExpr :: Poly -> Expr
-polyToExpr p | hasZero        = sPoly
-             | length(lP) > 1 = (BExp AddOp sPoly polList)
-  where
-      lP       = (toList p)      
-      hasZero  = (or [ t == 0 | t <- (drop 1 lP)])
-      sPoly    | (length lP) == 0 = (N 0)
-               | otherwise = (BExp MulOp (X ((length lP) - 1)) (N (head lP)))
-      polList  = (polyToExpr (fromList (drop 1 lP)))
+-- Creates a singular expression, for example 4x^2, 4, x^2 
+sExpr :: Int -> Int -> Expr
+sExpr i 1 = N i
+sExpr 1 l = X (l - 1)
+sExpr i l = (BExp MulOp (N i) (X (l - 1)))
 
--- Write (and check) a quickCheck property for this function similar to
--- question 6. 
+-- Iterates through list and builds an expression
+listP :: [Int] -> Expr 
+listP []          = N 0
+listP (0:(x1:[])) = (N x1)
+listP (1:(0:[]))  = (X 1)
+listP (0:xs)      = listP xs
+listP (x:xs)      | allZero   = sExpr x l
+                  | otherwise = (BExp AddOp (sExpr x l) (listP xs))
+  where 
+    l = length (x:xs)
+    allZero = and [ i == 0 | i <- xs]
+
+-- Converts polynomial into expression
+polyToExpr :: Poly -> Expr
+polyToExpr p = listP (toList p) 
+-- polyToExpr :: Poly -> Expr
+-- polyToExpr p | (or [ t == 0 | t <- (drop 1 lP)])  = sPoly
+--              | length(lP) > 1                     = (BExp AddOp (sPoly) (polyToExpr (fromList (drop 1 lP))))
+--   where
+--       lP = (toList p)
+--       sPoly | (length lP) == 0 = N 0
+--             | otherwise = (BExp MulOp (X ((length lP) - 1)) (N (head lP)))
+
+-- Checks that evaluated polynomial converted to expr gives the same value 
+--as before
 prop_polyToExpr x p = (eval x (polyToExpr p)) == (evalPoly x p)
 
 --------------------------------------------------------------------------------
 -- * A8
--- A function
--- that simplifies an expression by converting it to a polynomial
--- and back again
+-- Simplifies an expression by converting it to a polynomial and back again
 simplify :: Expr -> Expr
 simplify e = polyToExpr(exprToPoly e)
 
 --------------------------------------------------------------------------------
-e1 = (BExp AddOp (BExp MulOp (X 3) (N 3)) (BExp MulOp (X 3) (N 5)))
 
 -- * A9
--- Write a quickCheck property
+-- QuickCheck property that makes sure a simplified
+-- expression does not contain any "junk"
 prop_noJunk :: Expr -> Bool
-prop_noJunk (N 0)                    = False
-prop_noJunk (X 0)                    = False
-prop_noJunk (N _)                    = True
-prop_noJunk (X _)                    = True
-prop_noJunk (BExp AddOp (N 0) _)     = False
-prop_noJunk (BExp AddOp _ (N 0))     = False
-prop_noJunk (BExp MulOp (N 1) _)     = False
-prop_noJunk (BExp MulOp _ (N 1))     = False
-prop_noJunk (BExp MulOp (N _) (N _)) = False --fails for this
-prop_noJunk (BExp op e1 e2)          = (prop_noJunk e1) && (prop_noJunk e2)
---that checks that a simplified expression does not contain any "junk":
---where junk is defined to be multiplication by one or zero,
---addition of zero, addition or multiplication of numbers, or x to the
---power zero. (You may need to fix A7)
+prop_noJunk e = noJunk(simplify e)
+
+noJunk :: Expr -> Bool
+noJunk (X 0)                      = False
+noJunk (N _)                      = True
+noJunk (X _)                      = True
+noJunk (BExp AddOp (N 0) _)       = False
+noJunk (BExp AddOp _ (N 0))       = False
+noJunk (BExp MulOp (N 1) _)       = False
+noJunk (BExp MulOp _ (N 1))       = False
+noJunk (BExp MulOp (N 0) _)       = False
+noJunk (BExp MulOp _ (N 0))       = False
+noJunk (BExp AddOp (N e1) (N e2)) = False
+noJunk (BExp MulOp (N e1) (N e2)) = False
+noJunk (BExp op e1 e2)          = (noJunk e1) && (noJunk e2)
 
 --------------------------------------------------------------------------------
