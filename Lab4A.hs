ad6cecc7b20b3657ce29a155e652c69b414d9b99
gabbe-k <gabbekall@gmail.com>
2019-10-20 17:17:41 +0200

    Update Lab4A.hs

--- Lab4A.hs
+++ Lab4A.hs
@@ -42,8 +42,8 @@ instance Show Expr where
 -- Generates random expressions
 
 rSingle :: Gen Expr
-rSingle = do exp <- choose(0,9)
-             n <- choose(0,100)
+rSingle = do exp <- (arbitrary :: Gen Int)
+             n <-  (arbitrary :: Gen Int)
              t <- elements[(X 0),(N 0)]
              case t of
               (X _) -> (return(X exp))
@@ -54,12 +54,9 @@ rOperEx 0 = rSingle
 rOperEx n | n > 0 = do op <- elements [AddOp,MulOp]
                        e1 <- rSingle
                        e2 <- (rOperEx (n-1))
-                       case (BExp op e1 e2) of
-                        (BExp MulOp (N 1) e2) -> return(BExp op (N 2) e2)
-                        (BExp MulOp e1 (N 1)) -> return(BExp op e1 (N 2))
-                        (BExp op     e1   e2)  -> return(BExp op e1 e2)
+                       return(BExp op e1 e2)
 
-instance Arbitrary Expr where arbitrary = do r <- choose (0,5)
+instance Arbitrary Expr where arbitrary = do r <- choose(0,10)
                                              rOperEx r
 
 --------------------------------------------------------------------------------
@@ -80,7 +77,6 @@ exprToPoly (N n)                      = fromList [n]
 exprToPoly (X n)                      = fromList (1 : replicate n 0)
 exprToPoly (BExp AddOp e1 e2)         = (exprToPoly e1) + (exprToPoly e2)
 exprToPoly (BExp MulOp e1 e2)         = (exprToPoly e1) * (exprToPoly e2)
-        
 
 -- Checks that evaluating the polynomial you get from exprToPoly gives
 -- the same answer as evaluating the expression
@@ -91,32 +87,25 @@ prop_exprToPoly e x = (eval x e) == evalPoly x (exprToPoly e)
 -- * A7
 -- Creates a singular expression, for example 4x^2, 4, x^2 
 sExpr :: Int -> Int -> Expr
-sExpr i 1 = N i
+sExpr x 1 = N x
 sExpr 1 l = X (l - 1)
-sExpr i l = (BExp MulOp (N i) (X (l - 1)))
+sExpr x l = (BExp MulOp (N x) (X (l - 1)))
 
 -- Iterates through list and builds an expression
 listP :: [Int] -> Expr 
 listP []          = N 0
 listP (0:(x1:[])) = (N x1)
-listP (1:(0:[]))  = (X 1)
 listP (0:xs)      = listP xs
 listP (x:xs)      | allZero   = sExpr x l
                   | otherwise = (BExp AddOp (sExpr x l) (listP xs))
   where 
     l = length (x:xs)
     allZero = and [ i == 0 | i <- xs]
+    
 
 -- Converts polynomial into expression
 polyToExpr :: Poly -> Expr
 polyToExpr p = listP (toList p) 
--- polyToExpr :: Poly -> Expr
--- polyToExpr p | (or [ t == 0 | t <- (drop 1 lP)])  = sPoly
---              | length(lP) > 1                     = (BExp AddOp (sPoly) (polyToExpr (fromList (drop 1 lP))))
---   where
---       lP = (toList p)
---       sPoly | (length lP) == 0 = N 0
---             | otherwise = (BExp MulOp (X ((length lP) - 1)) (N (head lP)))
 
 -- Checks that evaluated polynomial converted to expr gives the same value 
 --as before
@@ -148,6 +137,6 @@ noJunk (BExp MulOp (N 0) _)       = False
 noJunk (BExp MulOp _ (N 0))       = False
 noJunk (BExp AddOp (N e1) (N e2)) = False
 noJunk (BExp MulOp (N e1) (N e2)) = False
-noJunk (BExp op e1 e2)          = (noJunk e1) && (noJunk e2)
+noJunk (BExp op e1 e2)            = (noJunk e1) && (noJunk e2)
 
 --------------------------------------------------------------------------------
